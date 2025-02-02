c9f878a405897c2b4da62d9dc64e21b17c8a7faf
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-02 21:12:53 +0800

    20250202.211253

--- docker-build/commit-docker-image.sh
+++ docker-build/commit-docker-image.sh
@@ -2,24 +2,32 @@
 
 new_version=$(date '+%Y%m%d.%H%M%S')
 script_dir=$(dirname "$0")
-yaml_file="$script_dir/docker-compose-template.yml"
+yaml_file="../app-build/$script_dir/docker-compose-template.yml"
+
+# ========
+
+CONTAINER_NAME=$(awk -F= '/^ *- CONTAINER_NAME=/ {gsub(/ /,"",$2); print $2}' "$yaml_file")
+
+echo "CONTAINER_NAME: $CONTAINER_NAME"
 
 # ========
 
 # Get the line starting with "image:"
 image_line=$(awk '/^ *image:/ {print $0}' "$yaml_file")
 
-# Extract the string before the last "-"
-image_config=$(echo "$image_line" | rev | cut -d'-' -f2- | rev)
+# Extract the string before the last ":"
+image_prefix=$(echo "$image_line" | rev | cut -d':' -f2- | rev)
 
 # Replace the line with the new version
-sed -i "s|^ *image:.*|${image_config}-${new_version}|" "$yaml_file"
-
-# ========
+sed -i "s|^ *image:.*|${image_prefix}:${CONTAINER_NAME}-${new_version}|" "$yaml_file"
 
 IMAGE_NAME=$(awk '/^ *image:/ {sub(/^ *image: */, ""); sub(/ *$/, ""); print $0}' "$yaml_file")
 
-CONTAINER_NAME=$(awk -F= '/^ *- CONTAINER_NAME=/ {gsub(/ /,"",$2); print $2}' "$yaml_file")
+echo "IMAGE_NAME: $IMAGE_NAME"
+
+return 0
+
+# ========
 
 docker tag ${CONTAINER_NAME} ${IMAGE_NAME}
 docker push "${IMAGE_NAME}"
